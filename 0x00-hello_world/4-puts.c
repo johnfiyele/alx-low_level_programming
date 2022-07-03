12bbf8b5690e9ebe4f5dac8dc3d74ced549e4ef4
johnfiyele <johnfiyele@gmail.com>
2022-07-03 18:56:53 +0100

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * 
- * Description: 'the program's description'
- * 
- * Return: Always 0 (Success)
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
