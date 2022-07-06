97605f8abaac58708280a32c8977c0ae6b6c4fc0
johnfiyele <johnfiyele@gmail.com>
2022-07-06 13:35:52 +0100

    0-putchar update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** 
+/**
 * main - prints putchar
 * Return: always 0
 */
@@ -15,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
 }
