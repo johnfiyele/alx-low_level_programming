0f827e0c3de03387d47015dc1257eced8bb6a5dd
johnfiyele <johnfiyele@gmail.com>
2022-07-08 03:13:34 +0100

    24 hours printing

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -24,18 +24,15 @@ void jack_bauer(void)
 			{
 				_putchar(' : ');
 				_putchar('0');
-				_putchar(j);
+				_putchar(j + '0');
 			}
-			else if (j < 10)
+			else if (j > 10)
 			{
+				_putchar(' : ');
 				_putchar((j / 10) + '0'));
 				_putchar((j % 10) + '0'));
 			}
-			{
-			_putchar(i);
-			_putchar(' : ');
-			_putchar(j);
-			 }
 		}
+		_putchar('\n');
 	}
 }
