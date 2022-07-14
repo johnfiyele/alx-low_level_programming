988a52503c047081a8cac569c88695501b6eccde
johnfiyele <johnfiyele@gmail.com>
2022-07-14 03:05:29 +0100

    reverse string updated

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,11 +8,19 @@
 */
 void rev_string(char *s)
 {
-	int i = 0;
+	int len, i, half;
+	char temp;
 
-	while (s[i])
+	for (len = 0; s[len] != '\0'; len++)
+	;
+	i = 0;
+	half = len / 2;
+
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
 		i++;
-	while (i--)
-		_putchar(s[i]);
-	_putchar('\n');
+	}
 }
